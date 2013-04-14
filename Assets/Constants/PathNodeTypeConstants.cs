f045462989953de5f0cde645f154c00b4abaedd8
Dan Ruscoe <danruscoe@gmail.com>
2013-04-13 17:53:00 -0700

    Added path node type constants.

---
+++ Assets/Constants/PathNodeTypeConstants.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class PathNodeTypeConstants
+{
+	public const string START = "start";
+	public const string END = "end";
+	public const string CONTINUE = "continue";
+}
