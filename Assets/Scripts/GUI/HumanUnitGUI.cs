4212df011c2e226f9067871bf8cbb734d5436a2a
Dan Ruscoe <danruscoe@gmail.com>
2013-04-14 10:28:11 -0700

    Human Unit GUI now contains preview image.

--- Assets/Scripts/GUI/HumanUnitGUI.cs
+++ Assets/Scripts/GUI/HumanUnitGUI.cs
@@ -10,15 +10,17 @@ public class HumanUnitGUI : MonoBehaviour
 	
 	public void OnGUI()
 	{
-		GUI.Box(new Rect(10, 10, 140, 120), "Human Unit");
+		GUI.Box(new Rect(10, 10, 200, 120), "Human Unit");
 		
-		GUI.Label(new Rect (20, 40, 60, 20), "Name:");
-		GUI.Label(new Rect (70, 40, 60, 20), this.human.name);
+		GUI.DrawTexture(new Rect (20, 40, 50, 50), this.human.previewTexture);
 		
-		GUI.Label(new Rect (20, 60, 60, 20), "Age:");
-		GUI.Label(new Rect (70, 60, 60, 20), this.human.age.ToString());
+		GUI.Label(new Rect (90, 40, 60, 20), "Name:");
+		GUI.Label(new Rect (140, 40, 60, 20), this.human.name);
 		
-		GUI.Label(new Rect (20, 80, 60, 20), "Cash:");
-		GUI.Label(new Rect (70, 80, 60, 20), this.human.cash.ToString());
+		GUI.Label(new Rect (90, 60, 60, 20), "Age:");
+		GUI.Label(new Rect (140, 60, 60, 20), this.human.age.ToString());
+		
+		GUI.Label(new Rect (90, 80, 60, 20), "Cash:");
+		GUI.Label(new Rect (140, 80, 60, 20), this.human.cash.ToString());
 	}
 }
