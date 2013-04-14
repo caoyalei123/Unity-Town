52ffb3ab653fab316663feddb4dbede693a42420
Dan Ruscoe <danruscoe@gmail.com>
2013-04-13 17:54:12 -0700

    Added Vehicle Unit script.

---
+++ Assets/Scripts/Units/VehicleUnit.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+
+/**
+ * Represents a vehicle unit.
+ * Vehicles travel along road-based paths through the town.
+ */
+public class VehicleUnit : Unit
+{
+	// The speed this vehicle moves at.
+	public float speed = 1.5f;
+	// The max distance to check ahead for path nodes.
+	public float pathNodeMaxDistance = 20f;
+	
+	private Vector3 directionForward = new Vector3(-1, 0, 0);
+	
+	private RaycastHit raycastHit;
+	
+	private PathNode targetNode;
+	private bool movingToNode = false;
+	
+	private Vector3 pathStartPoint;
+	private float pathStartTime;
+	private float pathLength = 0f;
+	private float pathCompletion = 0f;
+	
+	/**
+	 * Uses raycasting to detect path nodes.
+	 * Follows path if nodes are found.
+	 */
+	public void Update()
+	{
+		if (!this.movingToNode)
+		{
+			if (Physics.Raycast(this.transform.position, this.directionForward, out this.raycastHit))
+			{
+				Debug.DrawLine(this.transform.position, this.raycastHit.point, Color.cyan, 0.2f);
+				
+				if (this.raycastHit.collider.gameObject.tag == TagConstants.PATH_NODES)
+				{
+					Debug.Log("Vehicle detected path node.");
+					
+					PathNode pathNode = (PathNode) this.raycastHit.collider.gameObject.GetComponent("PathNode");
+					
+					this.moveToNode(pathNode);
+				}
+			}
+		}
+		else if (this.targetNode != null)
+		{
+			float pathLengthComplete = (Time.time - this.pathStartTime) * this.speed;
+			
+			this.pathCompletion = (pathLengthComplete / pathLength);
+			
+			this.transform.position = Vector3.Lerp(this.pathStartPoint, this.targetNode.transform.position, this.pathCompletion);
+			
+			if (this.pathCompletion >= 1)
+			{
+				this.onNodeReached();
+			}
+		}
+	}
+	
+	private void moveToNode(PathNode pathNode)
+	{
+		this.targetNode = pathNode;
+		
+		this.pathStartPoint = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+		this.pathStartTime = Time.time;
+		this.pathLength = Vector3.Distance(this.pathStartPoint, pathNode.transform.position);
+		this.pathCompletion = 0;
+		
+		this.movingToNode = true;
+	}
+	
+	private void onNodeReached()
+	{
+		Debug.Log("Vehicle reached target node.");
+		
+		this.targetNode = null;
+		
+		this.movingToNode = false;
+	}
+}
