3b60d3c0985f4407b4a32a19f5905b34cf3dd182
Dan Ruscoe <danruscoe@gmail.com>
2013-04-13 17:54:23 -0700

    Added Path Node script.

---
+++ Assets/Scripts/Nodes/PathNode.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+/**
+ * Represents a path node.
+ * Path nodes guide units through the town.
+ */
+public class PathNode : MonoBehaviour
+{
+	public string nodeType;
+}
