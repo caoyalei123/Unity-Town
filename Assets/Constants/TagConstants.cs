8c7be51910abc430525ff9b44fad6edc09e4ba01
Dan Ruscoe <danruscoe@gmail.com>
2013-04-13 17:52:27 -0700

    Added Path Nodes tag constant.

--- Assets/Constants/TagConstants.cs
+++ Assets/Constants/TagConstants.cs
@@ -3,4 +3,5 @@ public class TagConstants
 	public const string SHOPS = "Shops";
 	public const string CASH_MACHINES = "Cash Machines";
 	public const string HUMANS = "Humans";
+	public const string PATH_NODES = "Path Nodes";
 }
