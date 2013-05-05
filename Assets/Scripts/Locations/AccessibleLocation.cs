fdabe400d216819212f295194d11fd11455128f2
Dan Ruscoe <danruscoe@gmail.com>
2013-05-05 13:58:26 -0700

    Added active property to locations. Improved documentation.

--- Assets/Scripts/Locations/AccessibleLocation.cs
+++ Assets/Scripts/Locations/AccessibleLocation.cs
@@ -6,7 +6,11 @@ using System.Collections;
  */
 public abstract class AccessibleLocation : MonoBehaviour
 {
+	// The game object a unit must reach to interact
+	// with this location.
 	public GameObject accessPoint;
+	// True if this location is currently active.
+	public bool active = true;
 	
 	public virtual void Start()
 	{
@@ -18,5 +22,12 @@ public abstract class AccessibleLocation : MonoBehaviour
 	
 	}
 	
+	/**
+	 * Runs a location specific interaction between
+	 * this location and a human unit.
+	 * 
+	 * Interactions may affect the state of both the unit
+	 * and the location.
+	 */
 	public abstract void RunHumanInteraction(HumanUnit humanUnit);
 }
