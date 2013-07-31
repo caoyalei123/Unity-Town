528200942d5442089d7bf04627d3c4bf467f9887
Dan Ruscoe <danruscoe@gmail.com>
2013-07-30 19:42:46 -0700

    Added new constant for Units layer.

--- Assets/Constants/LayerConstants.cs
+++ Assets/Constants/LayerConstants.cs
@@ -3,4 +3,5 @@ public class LayerConstants
 	public const string GROUND = "Ground";
 	public const string OBSTACLES = "Obstacles";
 	public const string PATH_NODES = "Path Nodes";
+	public const string UNITS = "Units";
 }
