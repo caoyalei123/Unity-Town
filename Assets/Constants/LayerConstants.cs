78127e84a5140d17d6f6dc08475c37764e69cf90
Dan Ruscoe <danruscoe@gmail.com>
2013-08-30 23:46:47 -0700

    Added access point layer constant.

--- Assets/Constants/LayerConstants.cs
+++ Assets/Constants/LayerConstants.cs
@@ -4,4 +4,5 @@ public class LayerConstants
 	public const string OBSTACLES = "Obstacles";
 	public const string PATH_NODES = "Path Nodes";
 	public const string UNITS = "Units";
+	public const string ACCESS_POINTS = "Access Points";
 }
